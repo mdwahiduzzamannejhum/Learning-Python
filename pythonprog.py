10b75128c2f2802cd211882385b0e92de094a4e4
Md. Wahiduzzaman Nejhum <140197813+mdwahiduzzamannejhum@users.noreply.github.com>
2025-01-07 23:34:22 +0600

    Add files via upload

---
+++ pythonprog.py
@@ -0,0 +1,45 @@
+print("i am a cse student")
+print("Md. Wahiduzzaman Nejhum")
+print("Md. Wahiduzzaman Nejhum","i am enjoying python")
+print(25)
+name="shraddha"
+age=23
+price=25.99
+print("My name is ",name)
+print("My age is ",age)
+print("My product price is ",price)
+print(type(name))
+print(type(age))
+print(type(price))
+age=23
+old=False
+a=None
+print(type(age))
+print(type(old))
+print(type(a))
+#comments
+'''my wish is will write 3 line comments,,,...........................................................................'''
+#Arithmatic operator
+a=5
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)#remainder
+print(a**b)#a^b
+
+a=50
+b=20
+print(a==b)#False
+print(a!=b)#true
+a=50
+b=30
+print(not False)
+print(not(a>b))
+
+val1=True
+val2=True
+print("And Operator :",val1 and val2)
+print("OR Operator :",val1 or val2)
+print("OR Operator :",(a==b) or (a>b))
